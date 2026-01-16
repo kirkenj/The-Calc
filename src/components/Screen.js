7da6e62fb116a88efbfe53387274c4e14a956eef
kirkenj <kirill.pisaruk@gmail.com>
2026-01-16 16:44:30 +0100

    stateFix

---
+++ src/components/Screen.js
@@ -0,0 +1,8 @@
+
+const Screen = () => {
+    return (
+        <div className='screen'>23456789</div>
+    )
+}
+
+export default Screen
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,6 +1,5 @@
-import React from 'react'
 
-const Wrapper = ({children}) => {
+const Wrapper = ({ children }) => {
   return (
     <div className='wrapper'>{children}</div>
   )
