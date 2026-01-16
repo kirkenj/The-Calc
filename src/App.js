622cdad67dbe6b6ae562c4363cbc5d228a127a5f
kirkenj <kirill.pisaruk@gmail.com>
2026-01-16 13:21:49 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+    <div>
+      <Wrapper>
+        <h1>App</h1>  
+      </Wrapper>     
+    </div>
+  );
+}
+
+export default App;
